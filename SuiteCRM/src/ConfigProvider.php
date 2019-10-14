033c801a4e206648650990eb1b61deac1526f92c
Andrew Wallace <andrew.wallace@portospire.com>
2019-10-14 17:58:44 -0400

    -- initial commit

---
+++ SuiteCRM/src/ConfigProvider.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace SuiteCRM;
+
+/**
+ * The configuration provider for the App module
+ *
+ * @see https://docs.zendframework.com/zend-component-installer/
+ */
+class ConfigProvider
+{
+
+    /**
+     * Returns the configuration array
+     *
+     * To add a bit of a structure, each section is defined in a separate
+     * method which returns an array with its configuration.
+     *
+     */
+    public function __invoke(): array
+    {
+        return [
+            'dependencies' => $this->getDependencies(),
+            'templates' => $this->getTemplates(),
+        ];
+    }
+
+    /**
+     * Returns the container dependencies
+     */
+    public function getDependencies(): array
+    {
+        return [
+            'factories' => [
+                \App\Servics\SuiteCrm::class => \App\Service\SuiteCrmFactory::class,
+            ],
+        ];
+    }
+
+    /**
+     * Returns the templates configuration
+     */
+    public function getTemplates(): array
+    {
+        return [
+            'paths' => [
+            ],
+        ];
+    }
+
+}
