4776f9ba5b205fbde02055ab91d9a7d9e2b81f52
Andrew Wallace <andrew.wallace@portospire.com>
2019-10-15 13:28:11 -0400

    -- bumped version to 0.0.2 -- fixed incorrect name in composer file

--- PortoSpireSuiteCRMClient/src/Module.php
+++ PortoSpireSuiteCRMClient/src/Module.php
@@ -4,7 +4,7 @@ namespace Portospire\SuiteCRM;
 class Module
 {
 
-    const VERSION = "0.0.1";
+    const VERSION = "0.0.2";
  public function getConfig()
     {
         return include __DIR__ . '/../config/module.config.php';
