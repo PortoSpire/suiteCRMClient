bb275e0831e6f2e4e4e421a01d2fddfb39f1eb08
Andrew Wallace <andrew.wallace@portospire.com>
2019-10-15 15:01:59 -0400

    -- bumped version to 0.0.3 -- fixed psr4 notation

--- PortoSpireSuiteCRMClient/src/Module.php
+++ PortoSpireSuiteCRMClient/src/Module.php
@@ -4,7 +4,7 @@ namespace Portospire\SuiteCRM;
 class Module
 {
 
-    const VERSION = "0.0.2";
+    const VERSION = "0.0.3";
  public function getConfig()
     {
         return include __DIR__ . '/../config/module.config.php';
