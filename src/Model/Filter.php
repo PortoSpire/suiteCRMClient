fe27cafd735c545d983620fd4217ad9e1abcf4ea
Andrew Wallace <andrew.wallace@portospire.com>
2020-06-21 12:17:52 -0400

    -- added base object models and filters

---
+++ src/Model/Filter.php
@@ -0,0 +1,142 @@
+<?php
+
+/**
+ * Description of Filter
+ * 
+ * PHP version 7
+ * 
+ * * * License * * * 
+ * Copyright (C) 2019 PortoSpire, LLC.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ * * * End License * * * 
+ * 
+ * @category  CategoryName
+ * @package   PackageName
+ * @author    Andrew Wallace <andrew.wallace@portospire.com>
+ * @copyright 2019 PORTOSPIRE
+ * @license   LGPL 3
+ * @version   GIT: $ID$
+ * @link      https://portospire.com 
+ */
+
+namespace PortoSpire\SuiteCRMClient\Model;
+
+/**
+ * Description of Filter
+ *
+ * @category  CategoryName
+ * @package   PackageName
+ * @author    Andrew Wallace <andrew.wallace@portospire.com>
+ * @copyright 2019 PORTOSPIRE
+ * @license   LGPL 3
+ * @version   Release: @package_version@
+ * @link      https://coderepo.portospire.com/#git_repo_name
+ * @since     Class available since Release 0.0.0
+ */
+class Filter
+{
+    const _operators = ['and'=>'&','or'=>'||'],
+        _conditions = ['EQ'=>'=','NEQ'=>'!=','GT'=>'>','GTE'=>'>=','LT'=>'<','LTE'=>'<='];
+    public $operator,$field,$value,$condition;
+    //private $chainedFilterArray;
+    //private $chainedFilterString;
+    
+    public function __construct(array $definition=[],string $condition='EQ',string $operator='and')
+    {
+        if($definition !== []){
+            $this->condition = $this->checkCondition($condition);
+            $this->operator = $this->checkOperator($operator);
+            $this->field = key($definition);
+            $this->value = $definition[$this->field];
+        }
+    }
+    /*
+    private function buildChainedFilter($operator='and')
+    {
+        $this->buildChainedArray($operator);
+        $this->buildChainedString($operator);
+    }
+    
+    private function buildChainedString($operator='and')
+    {
+        $this->chainedFilterString = $this->chainedFilterString.'&'.$this->toString();
+    }
+    
+    private function buildChainedArray($operator='and')
+    {
+        $this->chainedFilterArray[] = $this->toArray();
+    }
+     * */
+    
+    public function and(Filter $filter)
+    {
+        
+    }
+    
+    public function or(Filter $filter)
+    {
+        
+    }
+    
+    public function export($type='array'){
+        $method = 'to'. ucfirst($type);
+        if(method_exists($this, $method)){
+            return $this->$method;
+        }
+    }
+    
+    public function toString()
+    {
+        $condition = $this->checkCondition($this->condition);
+        $operator = $this->checkOperator($this->operator);
+        return 'filter[operator]='.$operator.'&filter['.$this->field.']['.$condition.']='.$this->value;
+    }
+    
+    public function toArray()
+    {
+        $condition = $this->checkCondition($this->condition);
+        $operator = $this->checkOperator($this->operator);
+        return ['filter[operator]'=>$operator,'filter['.$this->field.']['.$condition.']'=>$this->value];
+    }
+    
+    public function __toString()
+    {
+        return $this->toString();
+    }
+    
+    private function checkOperator($operator)
+    {
+        if(array_key_exists($operator, $this::_operators)){
+            return $operator;
+        }
+        if($key = array_search($operator, $this::_operators)){
+            return $key;
+        }
+        return 'and'; // default to and
+    }
+    
+    private function checkCondition($condition)
+    {
+        if(array_key_exists($condition, $this::_conditions)){
+            return $condition;
+        }
+        if($key = array_search($condition, $this::_conditions)){
+            return $key;
+        }
+        return 'EQ'; //default to equals
+    }
+}
---
+++ src/Model/WebToPerson.php
@@ -0,0 +1,120 @@
+<?php
+
+/**
+ * Description of WebToPerson
+ * 
+ * PHP version 7
+ * 
+ * * * License * * * 
+ * Copyright (C) 2019 PortoSpire, LLC.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ * * * End License * * * 
+ * 
+ * @category  CategoryName
+ * @package   PackageName
+ * @author    Andrew Wallace <andrew.wallace@portospire.com>
+ * @copyright 2019 PORTOSPIRE
+ * @license   LGPL 3
+ * @version   GIT: $ID$
+ * @link      https://portospire.com 
+ */
+
+namespace PortoSpire\SuiteCRMClient\Model;
+
+/**
+ * Description of WebToPerson
+ *
+ * @category  CategoryName
+ * @package   PackageName
+ * @author    Andrew Wallace <andrew.wallace@portospire.com>
+ * @copyright 2019 PORTOSPIRE
+ * @license   LGPL 3
+ * @version   Release: @package_version@
+ * @link      https://coderepo.portospire.com/#git_repo_name
+ * @since     Class available since Release 0.0.0
+ */
+class WebToPerson
+{
+    const MODULE_DIR = 'Prospects';
+    public $description, $salutation, $first_name, $last_name, $title, $department,
+        $do_not_call, $phone_home, $phone_mobile, $phone_work, $phone_other, $phone_fax,
+        $email1, $email2, $lawful_basis, $date_reviewed, $lawful_basis_source,
+        $primary_address_street, $primary_address_city, $primary_address_state,
+        $primary_address_postalcode, $primary_address_country, $alt_address_street,
+        $alt_address_city, $alt_address_state, $alt_address_postalcode, $alt_address_country,
+        $assistant, $assistant_phone, $account_name, $birthdate;
+    public $salutation_values = ['Mr.','Ms.','Mrs.','Mrs.','Miss','Dr.','Prof.', ''],
+        $lawful_basis_values = ['consent', 'contract', 'legal_obligation',
+            'protection_of_interest','public_interest','legitimate_interest','withdrawn',''],
+        $lawful_basis_source_values = ['website','phone','given_to_user','email','third_party',''];
+    public $has_options = ['salutation','lawful_basis','lawful_basis_source'];
+    public $required_fields = ['last_name'];
+    
+    public function __construct()
+    {
+        //no constructing done at this time.
+    }
+    
+    public function extractNonEmpty(): array
+    {
+        $return = [];
+        $vars = get_object_vars($this);
+        foreach($vars as $key=>$value){
+            if(!empty($value) && !is_array($value)){
+                $return[$key]=$value;
+            }
+        }
+        return $return;
+    }
+    
+    public function toArray(): array
+    {
+        return get_object_vars($this);
+    }
+    
+    public function exchangeArray(array $data): WebToPerson
+    {
+        foreach($data as $key=>$value)
+        {
+            if(property_exists($this, $key)){
+                $this->$key = $value;
+            }
+        }
+        return $this;
+    }
+    
+    public function checkRequiredFields()
+    {
+        foreach($this->required_fields as $field)
+        {
+            if(empty($this->$field)){
+                throw \Exception('SuiteCRM: "'.$field.'" is a required field for WebToPerson submissions');
+            }
+        }
+    }
+    
+    public function checkFieldOptions()
+    {
+        foreach($this->has_options as $opt)
+        {
+            $key = $opt.'_values';
+            if(!in_array($this->$opt, $this->$key)){
+                throw new \Exception('SuiteCRM: "'.$this->$opt.'" is not a valid value for attribute "'.$opt.'"');
+            }
+        }
+    }
+}
