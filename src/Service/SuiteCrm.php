7148f7a63e5c8b6e712a36b320470a8a4095205a
andrewwallace <andrewwallace@popos-117.portospire.lan>
2026-01-05 21:35:44 -0500

    - bumped version to 0.1.5.1

--- src/Service/SuiteCrm.php
+++ src/Service/SuiteCrm.php
@@ -29,7 +29,7 @@
  * @author    Andrew Wallace <andrew.wallace@portospire.com>
  * @copyright 2026 PORTOSPIRE
  * @license   LGPL 3
- * @version   Release: 0.1.4.1
+ * @version   Release: 0.1.5.1
  * @link      https://portospire.github.io/
  */
 
@@ -57,7 +57,7 @@ use Swoole\MySQL\Exception as Exception2;
  * @author    Andrew Wallace <andrew.wallace@portospire.com>
  * @copyright 2026 PORTOSPIRE
  * @license   LGPL 3
- * @version   Release: 0.1.5.0
+ * @version   Release: 0.1.5.1
  * @link      https://portospire.github.io/
  * @since     Class available since Release 0.0.1
  */
